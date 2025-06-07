80cea5163b31a8b6c93e00e98af090831562def7
Dominika Kubacka <dominikakubacka368@gmail.com>
2025-06-07 11:40:11 +0200

    Zmiana formatu wywietlania daty

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ const displayArticles = (articles) => {
       <h2>${title}</h2>
       <h3>${subtitle}</h3>
       <p>Autor:${author}</p>
-      <p>Data utworzenia:${new Date(created_at).toLocaleString()}</p>
+      <p>Data utworzenia:${format(new Date(created_at).toLocaleString(), 'DD-MM-YYYY')}</p>
       <p>${content}</p>
       <hr />
     `;
