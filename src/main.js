6a2677bb6530d4000e26960086d125a104f921bf
Dominika Kubacka <dominikakubacka368@gmail.com>
2025-06-07 11:00:17 +0200

    Poprawki wywietlanie artykuów

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css'
 const fetchArticles = async () => {
  try {
  const response = await fetch(
- '<https://bhkmazgvcrgqejvvmqmy.supabase.co>', {
+ '<https://bhkmazgvcrgqejvvmqmy.supabase.co/rest/v1/articles>', {
  headers: {
  apiKey: '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoa21hemd2Y3JncWVqdnZtcW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTM5NjAsImV4cCI6MjA2MzIyOTk2MH0.7PT8Y-oEaOLRjMUz2Vc4IL7Mh1bGNzLqK-2k1lx98Lk>',
  },
@@ -67,14 +67,10 @@ const newArticle = {
     created_at: new Date().toISOString(),
   };
 
-  // Wysyłamy artykuł do Supabase
   await createNewArticle(newArticle);
 
-  // Odświeżamy listę artykułów
   const articles = await fetchArticles();
   displayArticles(articles);
-
-  // Czyścimy formularz
   form.reset();
 });
 
